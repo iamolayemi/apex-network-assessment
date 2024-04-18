b8fb1026797450059324286421f0b29d0b00fb15
Olatayo Olayemi <olatayo.olayemi.peter@gmail.com>
2024-04-18 20:25:49 +0100

    feat: set up laravel passport

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,9 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Enable mass assignment protection
         Model::unguard();
         Model::shouldBeStrict();
 
+        // Set the default password rules
         Password::defaults(function () {
             return $this->app->isProduction() ? Password::min(8)
                 ->letters()
@@ -24,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
                 ->symbols()
                 ->uncompromised() : Password::min(8);
         });
+
+        // Set the default token expiration times
+        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
     }
 }
